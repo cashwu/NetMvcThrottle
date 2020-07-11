49e93474850b1cb792db7a361b49526b8543548b
cash <cash@cashwu.com>
2020-07-11 11:20:15 +0800

    init

---
+++ testMvcThrottle/App_Start/FilterConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcThrottle;
+
+namespace testMvcThrottle
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            bool.TryParse(ConfigurationManager.AppSettings["IpRateLimitEnable"], out var ipRateLimitEnable);
+
+            if (ipRateLimitEnable)
+            {
+                var throttleFilter = new MvcThrottleCustomFilter
+                {
+                    Policy = new ThrottlePolicy(2, 30)
+                    {
+                        IpThrottling = true,
+                        StackBlockedRequests = true,
+                    },
+                    Repository = new CacheRepository(),
+                    QuotaExceededResponseCode = HttpStatusCode.OK
+                };
+
+                filters.Add(throttleFilter);
+            }
+        }
+    }
+
+    public class MvcThrottleCustomFilter : ThrottlingFilter
+    {
+        protected override ActionResult QuotaExceededResult(RequestContext filterContext, string message, HttpStatusCode responseCode, string requestId)
+        {
+            return new JsonResult
+            {
+                Data = new
+                {
+                    Error = "系統很忙 !!"
+                },
+               JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
---
+++ testMvcThrottle/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcThrottle;
+
+namespace testMvcThrottle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [EnableThrottling]
+        public ActionResult About(string aa)
+        {
+            ViewBag.Message = $"Your application description page. - {aa}";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
